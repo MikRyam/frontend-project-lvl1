8f287894bf47317b1a2cfd7540840a9b9de02338
Mikhail Ryamov <mikhailryamov@gmail.com>
2022-07-12 17:54:13 +0300

    add src/index.js

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+
+let userName = '';
+
+const greetings = () => {
+  console.log('Welcome to the Brain Games!');
+  userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi, ${userName}!`);
+  return userName;
+};
+
+const gameScenario = (gameRound) => {
+  const correctAnswer = gameRound;
+  const userAnswer = readlineSync.question('Your answer: ');
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+  }
+  return userAnswer === correctAnswer;
+};
+
+const congrats = () => console.log(`Congratulations, ${userName}!`);
+
+export { gameScenario, congrats };
+
+export default greetings;
